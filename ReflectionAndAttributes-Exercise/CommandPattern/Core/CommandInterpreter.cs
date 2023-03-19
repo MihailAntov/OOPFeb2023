d1a497a5e527a47f76909d54f8235bf654855bd1
MihailAntov <114488260+MihailAntov@users.noreply.github.com>
2023-03-19 10:07:41 +0200

    exercises added - reflection, unit tests

---
+++ ReflectionAndAttributes-Exercise/CommandPattern/Core/CommandInterpreter.cs
@@ -0,0 +1,30 @@
+﻿
+using System.Linq;
+using System;
+using System.Reflection;
+
+using CommandPattern.Core.Contracts;
+
+namespace CommandPattern.Core
+{
+    public class CommandInterpreter : ICommandInterpreter
+    {
+        public string Read(string args)
+        {
+            string[] inputArgs = args.Split(" ", System.StringSplitOptions.RemoveEmptyEntries);
+
+            string command = inputArgs[0];
+            string[] commandArgs = inputArgs.Skip(1).ToArray();
+
+            Type commandType = Assembly.GetEntryAssembly()
+                .GetTypes()
+                .FirstOrDefault(t => t.Name == $"{command}Command");
+
+            ICommand instance = (ICommand)Activator.CreateInstance(commandType);
+
+            string result = instance.Execute(commandArgs);
+
+            return result;
+        }
+    }
+}
---
+++ UnitTesting-Lab/Skeleton.Tests/DummyTests.cs
@@ -0,0 +1,51 @@
+﻿using NUnit.Framework;
+using System;
+
+namespace Skeleton.Tests
+{
+    [TestFixture]
+    public class DummyTests
+    {
+        [Test]
+        public void DummyLosesHealthIfAttacked()
+        {
+            const int StartingHealth = 10;
+            const int Attack = 5;
+            Dummy dummy = new Dummy(StartingHealth, 10);
+            dummy.TakeAttack(Attack);
+
+            Assert.That(dummy.Health, Is.EqualTo(StartingHealth - Attack),"Dummy is not losing health properly.");
+        }
+
+        [Test]
+        public void DeadDummyThrowsExceptionIfAttacked()
+        {
+            const int StartingHealth = 0;
+            const int Attack = 5;
+            Dummy dummy = new Dummy(StartingHealth, 10);
+
+            Assert.Throws<InvalidOperationException>(() => dummy.TakeAttack(Attack),"Dead dummy can't be attacked.");
+        }
+
+        [Test]
+        public void DeadDummyCanGiveXp()
+        {
+            const int StartingHealth = 0;
+            const int DummyXpReward = 10;
+            Dummy dummy = new Dummy(StartingHealth, DummyXpReward);
+            int xp = dummy.GiveExperience();
+
+            Assert.That(xp, Is.EqualTo(DummyXpReward),"A dead dummy can give xp");
+        }
+
+        [Test]
+        public void AliveDUmmyCantGiveXp()
+        {
+            const int StartingHealth = 5;
+            const int DummyXpReward = 10;
+            Dummy dummy = new Dummy(StartingHealth, DummyXpReward);
+
+            Assert.Throws<InvalidOperationException>(() => dummy.GiveExperience(),"An alive dummy can't give xp");
+        }
+    }
+}
